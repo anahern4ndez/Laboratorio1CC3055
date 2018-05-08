f2460d4ae8de7c44bdc9cf3f1e233c35e54565cd
estebancabreraa <esteban.cabrera.a@gmail.com>
2018-05-08 08:09:43 -0600

    Arreglo

--- Laboratorio1/app/src/main/java/com/plataformas/anahernandez/laboratorio1/Hospital.java
+++ Laboratorio1/app/src/main/java/com/plataformas/anahernandez/laboratorio1/Hospital.java
@@ -7,6 +7,7 @@ import android.os.Parcelable;
  * Esteban Cabrera 17781
  * Fernando Hengstenberg 17699
  * Raul Monzon 17014
+ * Kevin Macario 17369
  * 08/05/2017
  * Laboratorio 1: Plataformas Moviles y Juegos
  *
